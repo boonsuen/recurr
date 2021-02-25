2b1b49c9502318548e5f8b7c17826c68d86a4366
Boonsuen Oh <boonsuenoh@gmail.com>
2021-02-25 11:08:31 +0800

    fix: empty state view flash

--- app/src/main/java/com/boonsuen/recurr/fragments/SharedViewModel.kt
+++ app/src/main/java/com/boonsuen/recurr/fragments/SharedViewModel.kt
@@ -16,7 +16,7 @@ class SharedViewModel(application: Application): AndroidViewModel(application) {
 
     /** ============== List Fragment ============== */
 
-    val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(true)
+    val emptyDatabase: MutableLiveData<Boolean> = MutableLiveData(false)
 
     fun checkIfDatabaseEmpty(subscriptionData: List<SubscriptionData>) {
         emptyDatabase.value = subscriptionData.isEmpty()
